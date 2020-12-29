b1f78c9f6335dff7dfc78731b119a2a0dbf72e5e
Martinique Dolce <me@martidolce.com>
2020-12-29 10:28:20 -0500

    Spec Passes

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
-	end
-
-
+  get '/hello' do
+    erb :hello
+  end
+  get '/goodbye' do
+    erb :goodbye
+  end
+  get '/date' do
+    erb :date
+  end
 end
